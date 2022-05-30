5901d8b0517e340e0b777690263152e7ef54b1dd
DomingoCarvajal <domingo.carvajal@uc.cl>
2022-05-29 20:47:53 -0400

    feat(admin): users view

--- src/views/Admin.jsx
+++ src/views/Admin.jsx
@@ -4,7 +4,7 @@ function Admin() {
   const [users, setUsers] = useState(null);
   async function getUsers() {
     const url = 'http://localhost:3001';
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJhZ3N0ZXJAZW1haWwuY29tIiwiaWF0IjoxNjUzODQ2MDEwLCJleHAiOjE2NTM4NTMyMTB9.aS79jd7NbhKnvpAslQd3CRGMAUD5_zprIY_r17qYjcc';
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluM0BlbWFpbC5jb20iLCJpYXQiOjE2NTM4NzA3MjksImV4cCI6MTY1Mzg3NzkyOX0.aXdkdgLUKXfz46GoFA3eHHLtpwOSb3AwGaHnDSj6PJc';
     const responseAll = await fetch(`${url}/api/admin/users`, {
       method: 'GET',
       headers: { Authorization: `Bearer ${token}` },
@@ -16,43 +16,56 @@ function Admin() {
   }, []);
 
   console.log(users);
-  let nombre;
+  let testUsers;
   if (users == null) {
-    nombre = 'benja';
+    testUsers = [{
+      email: 'test@uc.cl',
+      firstName: 'TEst',
+      lastName: 'carvajal',
+    }];
   } else {
-    nombre = users[1].firstName;
+    testUsers = users;
   }
 
   return (
-    <div className="flex flex-row items-center justify-center space-x-0">
-      <div className="flex flex-col space-y-4 text-xl">
-        <table className="table-fixed">
-          <thead>
-            <tr>
-              <th>Song</th>
-              <th>Artist</th>
-              <th>Year</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{nombre}</td>
-              <td>Malcolm Lockyer</td>
-              <td>1961</td>
-            </tr>
-            <tr>
-              <td>Witchy Woman</td>
-              <td>The Eagles</td>
-              <td>1972</td>
-            </tr>
-            <tr>
-              <td>Shining Star</td>
-              <td>Earth, Wind, and Fire</td>
-              <td>1975</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+    <div className="py-12 grid bg-gray-300 grid-cols-3 gap-4">
+
+      {testUsers.map((value) => (
+        <div className="w-60 mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-md">
+          <div className="md:flex">
+            <div className="w-full p-2 py-10">
+
+              <div className="flex justify-center">
+                <div className="relative">
+                  <img
+                    src="https://i.imgur.com/z4YSzDD.jpg"
+                    className="rounded-full"
+                    width="80"
+                  />
+                  <span className="absolute border-white border-4 h-5 w-5 top-12 left-16 bg-green-700 rounded-full" />
+                </div>
+              </div>
+
+              <div className="flex flex-col text-center mt-3 mb-4">
+                <span className="text-2xl font-medium">
+                  {value.firstName}
+                  {' '}
+                  {value.lastName}
+                </span>
+                <span className="text-md text-gray-400">{value.email}</span>
+              </div>
+
+              <div className="px-14 mt-5">
+                <button className="h-12 bg-gray-200 w-full text-black text-md rounded hover:shadow hover:bg-gray-300 mb-2">Edit</button>
+                <button className="h-12 bg-blue-700 w-full text-white text-md rounded hover:shadow hover:bg-blue-800">Delete</button>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+      ))}
+
     </div>
   );
 }
